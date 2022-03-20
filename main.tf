ef69517534dad5bc09a9ca3b3bd56ef016800567
Paul Horton <phorton@sonatype.com>
2022-03-20 11:26:43 +0000

    feat: add LDAP and LDAPS rules to Security Groups between EKS Nodes and AWS Directory
    Signed-off-by: Paul Horton <phorton@sonatype.com>

--- main.tf
+++ main.tf
@@ -1,11 +1,11 @@
 module "shared_infrastructure" {
-  source      = "git::ssh://git@github.com/vendorcorp/terraform-shared-infrastructure.git?ref=v0.1.0"
+  source      = "git::ssh://git@github.com/vendorcorp/terraform-shared-infrastructure.git?ref=v0.2.1"
   environment = var.environment
 }
 
 resource "aws_directory_service_directory" "vendorcorp_ldap" {
-  name        = "vendorcorp.internal"
-  short_name  = "vendorcorp"
+  name        = "corp.vendorcorp.net"
+  short_name  = "corp"
   description = "Shared LDAP service for Vendor Corp"
   password    = local.ldap_admin_password
   edition     = "Standard"
@@ -20,7 +20,7 @@ resource "aws_directory_service_directory" "vendorcorp_ldap" {
   tags = var.default_resource_tags
 }
 
-resource "aws_route53_record" "dns_ldap_vendorcorp_intenral" {
+resource "aws_route53_record" "dns_ldap_vendorcorp_internal" {
   zone_id = module.shared_infrastructure.dns_zone_internal_id
   name    = "ldap.vendorcorp.internal"
   type    = "A"
@@ -34,4 +34,47 @@ resource "aws_route53_record" "dns_ldap_vendorcorp_public" {
   type    = "A"
   ttl     = "300"
   records = aws_directory_service_directory.vendorcorp_ldap.dns_ip_addresses
+}
+
+data "aws_eks_node_groups" "example" {
+  cluster_name = module.shared_infrastructure.eks_cluster_id
+}
+
+data "aws_launch_template" "templates" {
+  for_each = data.aws_eks_node_groups.example.names
+  tags = {
+    "eks:cluster-name" = module.shared_infrastructure.eks_cluster_id
+  }
+  filter {
+    name   = "tag:eks:nodegroup-name"
+    values = [each.value]
+  }
+}
+
+locals {
+  vpc_security_group_ids = distinct(flatten([
+    for template_name in keys(data.aws_launch_template.templates) : [
+      data.aws_launch_template.templates[template_name].vpc_security_group_ids
+    ]
+  ]))
+}
+
+resource "aws_security_group_rule" "allow_ldap" {
+  count                    = length(local.vpc_security_group_ids)
+  type                     = "egress"
+  from_port                = 389
+  to_port                  = 389
+  protocol                 = "tcp"
+  security_group_id        = local.vpc_security_group_ids[count.index]
+  source_security_group_id = aws_directory_service_directory.vendorcorp_ldap.security_group_id
+}
+
+resource "aws_security_group_rule" "allow_ldaps" {
+  count                    = length(local.vpc_security_group_ids)
+  type                     = "egress"
+  from_port                = 636
+  to_port                  = 636
+  protocol                 = "tcp"
+  security_group_id        = local.vpc_security_group_ids[count.index]
+  source_security_group_id = aws_directory_service_directory.vendorcorp_ldap.security_group_id
 }
